9bd19636a14baec001ea5740000f77e29deb4daa
Kuldeepsinh Parmar <kuldeepparmar92@gmail.com>
2023-09-11 20:42:17 +0530

    first commit

---
+++ src/main/java/com/javatechie/docker_jenkins_k8s_cicd_pipeline/DockerJenkinsK8sCicdPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkins_k8s_cicd_pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsK8sCicdPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsK8sCicdPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatechie/docker_jenkins_k8s_cicd_pipeline/DockerJenkinsK8sCicdPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker_jenkins_k8s_cicd_pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsK8sCicdPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
